d942d1ab2cc24de51c12c44eab8d001bacdcacfe
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2020-12-23 04:35:01 +0000

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,18 +3,17 @@
 
 sudo apt-get update
 sudo apt-get install -y nginx
-sudo mkdir /data
-sudo mkdir /data/web_static
-sudo mkdir /data/web_static/releases
-sudo mkdir /data/web_static/shared
-sudo mkdir /data/web_static/releases/test
-sudo echo "
-<html>
+sudo mkdir -p /data
+sudo mkdir -p /data/web_static
+sudo mkdir -p /data/web_static/releases
+sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test
+sudo echo "<html>
 <head></head>
 <body>
 <p>Test for web server nginx to deploy</p>
 </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -s /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
 sed -i '/listen 80 default_server;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
